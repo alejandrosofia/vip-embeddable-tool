add75d4ba1b967c0f3b1e99441f8757202bf6467
Alex Konrad <alexkonrad08@gmail.com>
2016-11-17 10:06:17 -0800

    Revert "add default electionId parameter"
    Because the presidential election is over, we now want the default
    election to be pulled. Fixes #230.
    
    This reverts commit afe6a602d5b59a9e32478e14c0af356976324cc5.

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -8,11 +8,7 @@ module.exports = function(options) {
   if (options.test) url += '&electionId=2000';
 
   if (!window._vitUsesStagingData) {
-    if (options.electionId) {
-      url += '&electionId=' + options.electionId;
-    } else {
-      url += '&electionId=5000';
-    }
+    if (options.electionId) url += '&electionId=' + options.electionId;
     if (options.officialOnly) url += '&officialOnly=' + options.officialOnly;
     if (typeof options.productionDataOnly !== 'undefined')
       url += '&productionDataOnly=' + options.productionDataOnly;
